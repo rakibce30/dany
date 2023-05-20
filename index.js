86290fa5221220b07520df0597e8adfb297ab087
rakibce <rakice30@gmail.com>
2023-05-20 11:39:36 +0600

    upload

--- index.js
+++ index.js
@@ -25,9 +25,17 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const toyCollection = client.db('danyDB').collection('toys');
+
+        // const index = await toyCollection.createIndex({name: 1});
+
+        // app.get('/toy', async(req, res)=>{
+        //     const result = await toyCollection.find({name: {$search: "Atlas"}})
+        //     console.log(result);
+        //     res.send({result});
+        // })
 
         // Toy GET request
         app.get('/toys', async(req, res) => {
